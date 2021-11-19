05bb1dfdf9630d3551fcc4279c6a300cbbf1e65b
saaketh9616 <saaketh89@gmail.com>
2021-11-19 19:28:09 +0530

    first commit

--- js/arrowfunc.js
+++ js/arrowfunc.js
@@ -1,5 +1,3 @@
-function add(a,b){
-    console.log(a+b);
-}
+const  add = (a,b) => console.log(a+b);
 
 add(10,20);
--- js/templateliteral.js
+++ js/templateliteral.js
@@ -3,3 +3,5 @@ let b = "IT";
 let c = "VIT";
 
 console.log("My Name is " + a + " , I study at " + c + " and my branch is "+ b);
+console.log(`My Name is ${a} , I study at ${c} and my branch is ${b}`);
+console.log(`${10*20 + 12*2}`);
